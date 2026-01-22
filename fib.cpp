bc4cb050defc41dd1bacd9eabdd0853c8609a529
akash <akash.karma17@gmail.com>
2026-01-22 21:51:26 +0530

    fibbonacci using memoization

---
+++ fib.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// using memoization
+
+int fib(int n, vector<int> &dp){
+    if(n<=1) return 1;
+    if(dp[n]!=-1) return dp[n];
+    return dp[n]=fib(n-1,dp)+fib(n-2,dp);
+}
+
+int main(){
+    int n=5;
+    vector<int> dp(n+1,-1);
+    cout<<fib(n,dp);
+}
